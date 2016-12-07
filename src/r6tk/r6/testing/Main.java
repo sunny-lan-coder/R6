cf3b09de3899d1c56a343eb81fe7a56c3cb343f2
sunny <sunny.lan.coder@gmail.com>
2016-12-07 08:55:56 -0500

    Continued testing of line

--- src/r6tk/r6/testing/Main.java
+++ src/r6tk/r6/testing/Main.java
@@ -7,9 +7,9 @@ import r6tk.r6.geom.Ray;
 public class Main {
 
 	public static void main(String[] args) throws R6Exception {
-		Ray r1=new Ray(1, 0, 0, true);
+		Ray r1=new Ray(0, 0, 0, true);
 		Ray r2=new Ray(1, 1, 0,false);
-		Line l1=new Line(0, 0, 10);
+		Line l1=new Line(5, 0, 10);
 		
 		System.out.println(l1.getXInt(r1));
 		System.out.println(l1.getYInt(r1));
