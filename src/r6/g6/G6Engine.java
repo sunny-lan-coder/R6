09816e742a2fb8708bbd2e70e16ad36ef8ee5af0
sunny <sunny.lan.coder@gmail.com>
2016-12-06 19:06:05 -0500

    dd

---
+++ src/r6/g6/G6Engine.java
@@ -0,0 +1,36 @@
+package r6.g6;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class G6Engine {
+	public final List<IRenderable> objects;
+	private Graphics g;
+	private double height;
+	private double width;
+
+	public G6Engine(double height, double width) {
+		objects = new ArrayList<>();
+		this.height = height;
+		this.width = width;
+	}
+
+	public void render(Graphics g) {
+		this.g = g;
+		for (IRenderable object : objects)
+			object.render(this);
+	}
+
+	public double height() {
+		return height;
+	}
+
+	public double width() {
+		return width;
+	}
+
+	public Graphics getGfx() {
+		return g;
+	}
+}
---
+++ src/r6/g6/TestWindow.java
@@ -0,0 +1,68 @@
+package r6.g6;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+public class TestWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestWindow window = new TestWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TestWindow() {
+		initialize();
+	}
+
+	private G6Engine engine=new G6Engine(450, 300);
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
+		JPanel panel = new JPanel(){
+
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 6677520948178100771L;
+			
+			@Override
+			public void paint(Graphics g){
+				engine.render(g);
+			}
+		};
+		
+		engine=new G6Engine(panel.getHeight(), panel.getWidth());
+		
+		
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+	}
+
+}
